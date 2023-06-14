b8ec3819e8611ef8aa78afaf6f298f2d1a8ec541
John Stanley Altonaga <johntaltonaga@gmail.com>
2023-06-14 20:00:16 +0800

    Connect routes

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -21,7 +17,13 @@ mongoose
     console.log(err);
   });
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/users", require("./routes/userRoute"));
+app.use("/parts", require("./routes/partRoute"));
+app.use("/carts", require("./routes/cartRoute"));
+app.use("/builds", require("./routes/buildRoute"));
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
